c394e63680b206a8d4319240023817332bb90cdb
Miguel <chanampamiguel@gmail.com>
2022-05-09 21:17:28 -0300

    Creación Servidor y Archivo de Rutas

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const routes = require('./routes');
+
+// Crear una app de express
+const app = express();
+
+// Rutas
+app.use('/', routes());
+
+app.listen(3000);
