b5d5ae27d36fe7dffba02807ab0750580b8cde2a
Miguel <chanampamiguel@gmail.com>
2022-05-09 22:10:22 -0300

    Creación Vistas

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const routes = require('./routes');
+const path = require('path');
 
 // Crear una app de express
 const app = express();
+
+// Habilitar Pug
+app.set('view engine', 'pug');
+
+// Añadir carpeta pública
+app.use(express.static('public'));
+
+// Añadir la carpeta de las vistas
+app.set('views', path.join(__dirname, './views'));
 
 // Rutas
 app.use('/', routes());
