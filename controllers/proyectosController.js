6be488ac5e05ca4437a787044cfb7d0200034266
Miguel <chanampamiguel@gmail.com>
2022-05-09 22:45:50 -0300

    Creando Proyectos

--- controllers/proyectosController.js
+++ controllers/proyectosController.js
@@ -3,3 +3,32 @@ exports.proyectosHome = (req, res) => {
         nombrePagina: 'Proyectos'
     });
 };
+
+exports.formularioProyecto = (req, res) => {
+    res.render('nuevoProyecto', {
+        nombrePagina: 'Nuevo Proyecto'
+    });
+};
+
+exports.nuevoProyecto = (req, res) => {
+
+    // Validar que tengamos algo en el input
+    const { nombre } = req.body;
+
+    let errores = [];
+
+    if (!nombre) {
+        errores.push({'texto': 'Agrega un nombre al proyecto'});
+    };
+
+    // Si hay errores
+    if (errores.length > 0) {
+        res.render('nuevoProyecto', {
+            nombrePagina: 'Nuevo Proyecto',
+            errores
+        });
+    } else {
+        // No hay errores
+        // Insertar en la BD
+    };
+};
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 // Importar el controlador
-const { proyectosHome } = require('../controllers/proyectosController');
+const { proyectosHome, formularioProyecto, nuevoProyecto } = require('../controllers/proyectosController');
 
 module.exports = function() {
     // Rutas
     router.get('/', proyectosHome);
+    router.get('/nuevo-proyecto', formularioProyecto);
+    router.post('/nuevo-proyecto', nuevoProyecto);
 
     return router;
 };
