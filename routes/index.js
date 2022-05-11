3d51cc98e96af90ddaf0babf77523d40778b73e5
Miguel <chanampamiguel@gmail.com>
2022-05-11 09:37:29 -0300

    Validacion Pre-Insertar en BD

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-
+// Importar express-validator
+const { body } = require('express-validator/check');
 // Importar el controlador
 const { proyectosHome, formularioProyecto, nuevoProyecto } = require('../controllers/proyectosController');
 
@@ -8,7 +9,10 @@ module.exports = function() {
     // Rutas
     router.get('/', proyectosHome);
     router.get('/nuevo-proyecto', formularioProyecto);
-    router.post('/nuevo-proyecto', nuevoProyecto);
+    router.post('/nuevo-proyecto',
+        body('nombre').not().isEmpty().trim().escape(),
+        nuevoProyecto
+    );
 
     return router;
 };
