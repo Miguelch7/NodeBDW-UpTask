956765b037039bb077e0a26d6751f33768a29de3
Miguel <chanampamiguel@gmail.com>
2022-05-22 17:34:45 -0300

    Controlar si el usuario está autenticado

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,3 +6,12 @@ exports.autenticarUsuario = passport.authenticate('local', {
     failureFlash: true,
     badRequestMessage: 'Ambos campos son obligatorios'
 });
+
+exports.usuarioAutenticado = (req, res, next) => {
+    
+    if (!req.isAuthenticated()) {
+        return res.redirect('/iniciar-sesion');
+    };
+    
+    return next();
+};
