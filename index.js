040f6022427bde4055f4cbccb3885d158cf53d69
Miguel <chanampamiguel@gmail.com>
2022-05-22 01:17:33 -0300

    Crear modelo Usuario

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const db = require('./config/db');
 // Importar los modelos
 require('./models/Proyectos');
 require('./models/Tareas');
+require('./models/Usuarios');
 
 db.sync()
     .then(() => console.log('Conectado a la BD'))
