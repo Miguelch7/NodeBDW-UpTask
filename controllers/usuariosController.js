f09131bc964043e7b0450dd4740e2ff0ad9d3cc3
Miguel <chanampamiguel@gmail.com>
2022-05-22 16:49:17 -0300

    Autorellenando el form en caso de errores

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -20,7 +20,9 @@ exports.crearCuenta = async (req, res) => {
         req.flash('error', error.errors.map( error => error.message ));
         res.render('crearCuenta', {
             nombrePagina: 'Crear Cuenta en UpTask',
-            mensajes: req.flash()
+            mensajes: req.flash(),
+            email,
+            password
         });
     };
 
