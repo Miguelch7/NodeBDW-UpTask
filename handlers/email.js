d1f51e773047fd13b0457e32a3fe93950e4f3c7e
Miguel <chanampamiguel@gmail.com>
2022-05-23 12:04:06 -0300

    Configurando Nodemailer

---
+++ handlers/email.js
@@ -0,0 +1,36 @@
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+const juice = require('juice');
+const { fromString } = require('html-to-text');
+const util = require('util');
+require('dotenv').config();
+
+let transport = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+const generarHTML = (archivo, opciones = {}) => {
+    const html = pug.renderFile(`${ __dirname }/../views/emails/${ archivo }.pug`, opciones);
+    return juice(html);
+};
+
+exports.enviar = async ({ subject, archivo, usuario, resetUrl }) => {
+
+    const html = generarHTML(archivo, { subject, archivo, usuario, resetUrl });
+    const text = fromString(html);
+
+    const enviarEmail = util.promisify(transport.sendMail, transport);
+
+    return enviarEmail.call(transport, {
+        from: 'UpTask <no-reply@uptask.com>',
+        to: usuario.email,
+        subject,
+        text,
+        html
+    });
+};
